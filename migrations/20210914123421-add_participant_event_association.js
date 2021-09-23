0007e3a7c083cd7f3e0e71d1280ebc0cd1715828
AlvinJK <ajk.alvin@gmail.com>
2021-09-23 16:15:36 +0700

    Added migrations from LDS

---
+++ migrations/20210914123421-add_participant_event_association.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @typedef {import('sequelize').Sequelize} Sequelize
+ * @typedef {import('sequelize').QueryInterface} QueryInterface
+ */
+module.exports = {
+  /**
+   * @param {QueryInterface} queryInterface
+   * @param {Sequelize} Sequelize
+   * @returns
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn(
+      'Participants',
+      'calendarEventId',
+      Sequelize.UUID,
+    );
+  },
+  /**
+   * @param {QueryInterface} queryInterface
+   * @param {Sequelize} Sequelize
+   * @returns
+   */
+  down: async (queryInterface) => {
+    await queryInterface.removeColumn('Participants', 'calendarEventId');
+  },
+};
