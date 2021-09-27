21967c8505895b7da420872182722663896b885f
Aan Fadhil <aanfadhil16@gmail.com>
2021-09-27 14:43:21 +0700

    Test/sns 416 2 (#3)
    * Add analysis engine models (#1)
    
    * Add missing analysis engine models
    
    * Move everything into a folder
    
    * Added migrations from LDS
    
    * add timeszone for calendar event and race
    
    * bugfix
    
    * add model diagram
    
    * scope course to event add name
    
    * add version
    
    Co-authored-by: Alvin Kurniawan <ajk.alvin@gmail.com>

---
+++ migrations/20210924014632-add_course_name_and_scope_to_event.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @typedef {import('sequelize').Sequelize} Sequelize
+ * @typedef {import('sequelize').QueryInterface} QueryInterface
+ */
+module.exports = {
+  /**
+   * @param {QueryInterface} queryInterface
+   * @param {Sequelize} Sequelize
+   * @returns
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn(
+      'Courses',
+      'calendarEventId',
+      Sequelize.UUID,
+    );
+    await queryInterface.addColumn('Courses', 'name', Sequelize.STRING);
+  },
+  /**
+   * @param {QueryInterface} queryInterface
+   * @param {Sequelize} Sequelize
+   * @returns
+   */
+  down: async (queryInterface) => {
+    await queryInterface.removeColumn('Courses', 'calendarEventId');
+    await queryInterface.removeColumn('Courses', 'name');
+  },
+};
