f42dc65b16a36db52fb0a7e9deddf8a326ffe741
aanfadhil <aanfadhil16@gmail.com>
2021-10-07 23:13:32 +0700

    add competition unit status

---
+++ migrations/20211007161032-add_race_status.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @typedef {import('sequelize').Sequelize} Sequelize
+ * @typedef {import('sequelize').QueryInterface} QueryInterface
+ */
+module.exports = {
+  /**
+   * @param {QueryInterface} queryInterface
+   * @param {Sequelize} Sequelize
+   * @returns
+   */
+  up: async (queryInterface, Sequelize) => {
+    const tableInfo = await queryInterface.describeTable('CompetitionUnits');
+
+    if (!tableInfo.status) {
+      await queryInterface.addColumn(
+        'CompetitionUnits',
+        'status',
+        Sequelize.UUID,
+      );
+    }
+  },
+  /**
+   * @param {QueryInterface} queryInterface
+   * @param {Sequelize} Sequelize
+   * @returns
+   */
+  down: async (queryInterface) => {
+    await queryInterface.removeColumn('CompetitionUnits', 'status');
+  },
+};
