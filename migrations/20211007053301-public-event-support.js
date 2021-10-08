8e60d2080773a218277dcfe529e0a62b19ad42a6
AlvinJK <ajk.alvin@gmail.com>
2021-10-08 09:22:14 +0700

    Added migration to add index (working index for query) + remove lon lat

--- migrations/20211007053301-public-event-support.js
+++ migrations/20211007053301-public-event-support.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const db = require('..');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     /**
@@ -9,8 +11,8 @@ module.exports = {
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
     await queryInterface.sequelize.transaction(async (transaction) => {
-      const compInfo = await queryInterface.describeTable('CalendarEvents');
-      if (!compInfo.isOpen) {
+      const eventInfo = await queryInterface.describeTable('CalendarEvents');
+      if (!eventInfo.isOpen) {
         await queryInterface.addColumn(
           'CalendarEvents',
           'isOpen',
@@ -18,18 +20,46 @@ module.exports = {
           { transaction },
         );
       }
-      if (!compInfo.location) {
+      if (!eventInfo.location) {
         await queryInterface.addColumn(
           'CalendarEvents',
           'location',
           Sequelize.GEOMETRY('POINT', 4326),
           { transaction },
         );
+        await db.sequelize.query(
+          'UPDATE "CalendarEvents" SET "location" = ST_SetSRID(ST_MakePoint("lon", "lat"), 4326)',
+          { transaction },
+        );
+        await queryInterface.removeColumn('CalendarEvents', 'lon', {
+          transaction,
+        });
+        await queryInterface.removeColumn('CalendarEvents', 'lat', {
+          transaction,
+        });
+        await queryInterface.addIndex('CalendarEvents', ['location'], {
+          unique: false,
+          type: 'SPATIAL',
+          using: 'GIST',
+          name: 'calendar_event_location_index',
+          transaction,
+        });
       }
+      await queryInterface.addIndex(
+        'CompetitionUnits',
+        ['approximateStartLocation'],
+        {
+          unique: false,
+          type: 'SPATIAL',
+          using: 'GIST',
+          name: 'competition_start_location_index',
+          transaction,
+        },
+      );
     });
   },
 
-  down: async (queryInterface) => {
+  down: async (queryInterface, Sequelize) => {
     /**
      * Add reverting commands here.
      *
@@ -40,9 +70,25 @@ module.exports = {
       await queryInterface.removeColumn('CalendarEvents', 'isOpen', {
         transaction,
       });
+      await queryInterface.addColumn('CalendarEvents', 'lon', Sequelize.FLOAT, {
+        transaction,
+      });
+      await queryInterface.addColumn('CalendarEvents', 'lat', Sequelize.FLOAT, {
+        transaction,
+      });
       await queryInterface.removeColumn('CalendarEvents', 'location', {
         transaction,
       });
+      await queryInterface.removeIndex(
+        'CalendarEvents',
+        'calendar_event_location_index',
+        { transaction },
+      );
+      await queryInterface.removeIndex(
+        'CompetitionUnits',
+        'competition_start_location_index',
+        { transaction },
+      );
     });
   },
 };
