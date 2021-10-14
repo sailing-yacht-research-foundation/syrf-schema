72e6c194e7aedddfc979dc669b470b68cb140311
Tung Nguyen <nguyenmanhtung848@gmail.com>
2021-10-14 15:53:06 +0700

    SNS-518: Update utils

--- utils/utils.js
+++ utils/utils.js
@@ -85,8 +85,8 @@ exports.AuthError = AuthError;
 exports.setCreateMeta = (data = {}, user) => {
   const now = Date.now();
 
-  data.createdById = user?.id;
-  data.developerId = user?.developerId;
+  data.createdById = user.id;
+  data.developerId = user.developerId;
   data.createdAt = now;
 
   return data;
@@ -95,7 +95,7 @@ exports.setCreateMeta = (data = {}, user) => {
 exports.setUpdateMeta = (data = {}, user) => {
   const now = Date.now();
 
-  data.updatedById = user?.id;
+  data.updatedById = user.id;
   data.updatedAt = now;
 
   return data;
@@ -110,20 +110,181 @@ exports.getMeta = ({ updatedById, updatedAt, createdById, createdAt } = {}) => {
   };
 };
 
+exports.validateSqlDataAuth = ({ editors = [], ownerId = '' } = {}, userId) => {
+  let result = {
+    isOwner: false,
+    isEditor: false,
+  };
+
+  if (Array.isArray(editors) && editors.length > 0) {
+    const idIndex = editors.findIndex((t) => t.id === userId);
+    result.isEditor = idIndex > -1;
+  }
+
+  result.isOwner = ownerId === userId;
+
+  return result;
+};
+
+exports.statusCodes = {
+  CONTINUE: 100,
+  SWITCHING_PROTOCOLS: 101,
+  PROCESSING: 102,
+  OK: 200,
+  CREATED: 201,
+  ACCEPTED: 202,
+  NON_AUTHORITATIVE_INFORMATION: 203,
+  NO_CONTENT: 204,
+  RESET_CONTENT: 205,
+  PARTIAL_CONTENT: 206,
+  MULTI_STATUS: 207,
+  MULTIPLE_CHOICES: 300,
+  MOVED_PERMANENTLY: 301,
+  MOVED_TEMPORARILY: 302,
+  SEE_OTHER: 303,
+  NOT_MODIFIED: 304,
+  USE_PROXY: 305,
+  TEMPORARY_REDIRECT: 307,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  PAYMENT_REQUIRED: 402,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  METHOD_NOT_ALLOWED: 405,
+  NOT_ACCEPTABLE: 406,
+  PROXY_AUTHENTICATION_REQUIRED: 407,
+  REQUEST_TIMEOUT: 408,
+  CONFLICT: 409,
+  GONE: 410,
+  LENGTH_REQUIRED: 411,
+  PRECONDITION_FAILED: 412,
+  REQUEST_ENTITY_TOO_LARGE: 413,
+  REQUEST_URI_TOO_LARGE: 414,
+  UNSUPPORTED_MEDIA_TYPE: 415,
+  REQUESTED_RANGE_NOT_SATISFIABLE: 416,
+  EXPECTATION_FAILED: 417,
+  IM_A_TEAPOT: 418,
+  UNPROCESSABLE_ENTITY: 422,
+  LOCKED: 423,
+  FAILED_DEPENDENCY: 424,
+  UNORDERED_COLLECTION: 425,
+  UPGRADE_REQUIRED: 426,
+  PRECONDITION_REQUIRED: 428,
+  TOO_MANY_REQUESTS: 429,
+  REQUEST_HEADER_FIELDS_TOO_LARGE: 431,
+  INTERNAL_SERVER_ERROR: 500,
+  NOT_IMPLEMENTED: 501,
+  BAD_GATEWAY: 502,
+  SERVICE_UNAVAILABLE: 503,
+  GATEWAY_TIME_OUT: 504,
+  HTTP_VERSION_NOT_SUPPORTED: 505,
+  VARIANT_ALSO_NEGOTIATES: 506,
+  INSUFFICIENT_STORAGE: 507,
+  BANDWIDTH_LIMIT_EXCEEDED: 509,
+  NOT_EXTENDED: 510,
+  NETWORK_AUTHENTICATION_REQUIRED: 511,
+};
+
+/**
+ * @typedef {import('express').Request} Request
+ * @typedef {import('express').Response} Response
+ * @typedef {import('express').NextFunction} NextFunction
+ *
+ */
+
+/**
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+const callBackType = (req, res, next) => { };
+
+/**
+ * @param {callBackType} fn
+ */
+exports.asyncHandler = (fn) =>
+  function (...args) {
+    const fnReturn = fn(...args);
+    const next = args[args.length - 1];
+    return Promise.resolve(fnReturn).catch(next);
+  };
+
+exports.includeMeta = [
+  {
+    model: db.UserProfile,
+    as: 'createdBy',
+    attributes: ['id', 'name'],
+  },
+  {
+    model: db.UserProfile,
+    as: 'updatedBy',
+    attributes: ['id', 'name'],
+  },
+];
+
+// 5 minutes default buffer
+exports.getRaceTimeBuffer = (time, buffer = 60000) => {
+  if (isNaN(time?.getTime())) return null;
+
+  return {
+    start: time,
+    end: new Date(time.getTime() + buffer),
+  };
+};
+
 exports.useTransaction =
   (fc) =>
-  async (...args) => {
-    if (args.length > 0 && args[args.length - 1] instanceof Transaction) {
-      return await fc(...args);
-    } else {
-      const tran = await db.sequelize.transaction();
-      try {
-        const result = await fc(...[...args, tran]);
-        await tran.commit();
-        return result;
-      } catch (error) {
-        await tran.rollback();
-        throw error;
+    async (...args) => {
+      if (args.length > 0 && args[args.length - 1] instanceof Transaction) {
+        return await fc(...args);
+      } else {
+        const tran = await db.sequelize.transaction();
+        try {
+          const result = await fc(...[...args, tran]);
+          await tran.commit();
+          return result;
+        } catch (error) {
+          await tran.rollback();
+          throw error;
+        }
       }
-    }
-  };
+    };
+
+exports.getCourseCenterPoint = (geometries = []) => {
+  if (geometries.length < 1) return null;
+
+  let centerPoint = null;
+
+  let { geometryType, coordinates } = geometries[0];
+  coordinates = coordinates.map((t) => (!Array.isArray(t) ? t.position : t)); //adjust coordinates to use array
+
+  // CourseSequencedGeometry created on LDS has different structure from geojson.io (and turf formatting),
+  // such as: Point is nested in another array, also simple line is treated as polyline
+  // Below implementation is based on LDS format
+  switch (String(geometryType).toLowerCase()) {
+    case 'line':
+      centerPoint = turf.midpoint(
+        coordinates[0],
+        coordinates[coordinates.length - 1],
+      ).geometry.coordinates;
+      break;
+    case 'polyline':
+      centerPoint = turf.midpoint(
+        coordinates[0],
+        coordinates[coordinates.length - 1],
+      ).geometry.coordinates;
+      break;
+    case 'polygon':
+      centerPoint = turf.centroid(turf.helpers.multiPoint(coordinates)).geometry
+        .coordinates;
+      break;
+    case 'point':
+      centerPoint = coordinates[0]; // Note: Based on the data found on our DB, point structure is different from geojson.io representation
+      break;
+    default:
+      centerPoint = null;
+      break;
+  }
+
+  return centerPoint;
+};
