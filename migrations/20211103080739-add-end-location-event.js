59e82893ecf464d26c13306ba41dfebbbba0d0fd
AlvinJK <ajk.alvin@gmail.com>
2021-11-03 15:22:53 +0700

    Also remove lon, lat from events

--- migrations/20211103080739-add-end-location-event.js
+++ migrations/20211103080739-add-end-location-event.js
@@ -2,21 +2,57 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const tableInfo = await queryInterface.describeTable('CalendarEvents');
-
-    if (!tableInfo.endLocation) {
-      await queryInterface.addColumn('CalendarEvents', 'endLocation', {
-        type: Sequelize.GEOMETRY('POINT', 4326),
-        allowNull: true,
-      });
-    }
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const tableInfo = await queryInterface.describeTable('CalendarEvents');
+      if (!tableInfo.endLocation) {
+        await queryInterface.addColumn(
+          'CalendarEvents',
+          'endLocation',
+          Sequelize.GEOMETRY('POINT', 4326),
+          { transaction },
+        );
+      }
+      if (tableInfo.lon) {
+        await queryInterface.removeColumn('CalendarEvents', 'lon', {
+          transaction,
+        });
+      }
+      if (tableInfo.lat) {
+        await queryInterface.removeColumn('CalendarEvents', 'lat', {
+          transaction,
+        });
+      }
+    });
   },
 
-  down: async (queryInterface) => {
-    const tableInfo = await queryInterface.describeTable('CalendarEvents');
-
-    if (tableInfo.endLocation) {
-      await queryInterface.removeColumn('CalendarEvents', 'endLocation');
-    }
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const tableInfo = await queryInterface.describeTable('CalendarEvents');
+      if (tableInfo.endLocation) {
+        await queryInterface.removeColumn('CalendarEvents', 'endLocation', {
+          transaction,
+        });
+      }
+      if (!tableInfo.lon) {
+        await queryInterface.addColumn(
+          'CalendarEvents',
+          'lon',
+          Sequelize.FLOAT,
+          {
+            transaction,
+          },
+        );
+      }
+      if (!tableInfo.lat) {
+        await queryInterface.addColumn(
+          'CalendarEvents',
+          'lat',
+          Sequelize.FLOAT,
+          {
+            transaction,
+          },
+        );
+      }
+    });
   },
 };
