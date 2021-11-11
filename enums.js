3c63cb874f90870fd1f998f3cf575f8f63dc7009
aanfadhil <aanfadhil16@gmail.com>
2021-11-11 20:50:30 +0700

    add queue

--- enums.js
+++ enums.js
@@ -141,6 +141,8 @@ const miscOptionsValue = {
 const bullQueues = {
   raceStart: 'race_start',
   expeditionSubsExpiration: 'expedition_subs_expiration',
+  openGraph: 'open_graph',
+  closestCity: 'closest_city',
 };
 
 module.exports = {
