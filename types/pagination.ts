7cdfaaddc4cf803a8d68a267b7eb447250be3321
Aan Fadhil <aanfadhil16@gmail.com>
2021-12-06 20:40:42 +0700

    Bugfix : new open event not appear in nearby search (#69)
    * add includeAttributes in geAdminsById
    also adding types to create jsdoc
    
    * update version
    
    * add LATEST_PP_VERSION redis key enum

---
+++ types/pagination.ts
@@ -0,0 +1,29 @@
+export interface PaginationFilter {
+  field: string,
+  value: any,
+  opr: string
+}
+
+export interface PaginationRequest {
+  query: string,
+  page: number,
+  size: number,
+  sort: string,
+  draw: number,
+  srdir: -1 | 1,
+  multiSort: Array<[string,'ASC'|'DESC']>,
+  filters: PaginationFilter[],
+}
+
+export interface PaginationResponse<T> {
+  count: number;
+  rows: Array<T>;
+  page: number;
+  size: number;
+  sort: string;
+  srdir: string;
+  q: string;
+  multiSort: Array<[string,'ASC'|'DESC']>,
+  filters: PaginationFilter[],
+
+}
