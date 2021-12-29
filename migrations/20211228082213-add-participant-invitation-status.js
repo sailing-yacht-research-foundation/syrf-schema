0c7b9e54ed8378559c59add329ad067285a1c929
AanFadhil <aanfadhil16@gmail.com>
2021-12-29 14:57:00 +0700

    adjust migration

--- migrations/20211228082213-add-participant-invitation-status.js
+++ migrations/20211228082213-add-participant-invitation-status.js
@@ -1,10 +1,20 @@
 'use strict';
 
 const { participantInvitationStatus } = require('../enums');
+const db = require('../index');
 
 const tableName = 'Participants';
 
+/**
+ * @typedef {import('sequelize').Sequelize} Sequelize
+ * @typedef {import('sequelize').QueryInterface} QueryInterface
+ */
 module.exports = {
+  /**
+   * @param {QueryInterface} queryInterface
+   * @param {Sequelize} Sequelize
+   * @returns
+   */
   up: async (queryInterface, Sequelize) => {
     const table = await queryInterface.describeTable(tableName);
 
@@ -14,11 +24,32 @@ module.exports = {
           Object.values(participantInvitationStatus),
         ),
       });
+
+      // update previous data to be accepted to prevent breaks
+      await db.Participant.update(
+        {
+          invitationStatus: participantInvitationStatus.ACCEPTED,
+        },
+        {
+          where: {
+            invitationStatus: null,
+          },
+        },
+      );
     }
   },
+  /**
+   * @param {QueryInterface} queryInterface
+   * @param {Sequelize} Sequelize
+   * @returns
+   */
   down: async (queryInterface) => {
     const table = await queryInterface.describeTable(tableName);
-    if (table.invitationStatus)
+    if (table.invitationStatus) {
       await queryInterface.removeColumn(tableName, 'invitationStatus');
+      await queryInterface.sequelize.query(
+        'DROP TYPE IF EXISTS "enum_Participants_invitationStatus";',
+      );
+    }
   },
 };
