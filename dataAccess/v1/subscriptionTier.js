7f15e56d0a5edbe7396b7a0dc51dfcef3c134ff5
AlvinJK <ajk.alvin@gmail.com>
2021-12-30 16:29:35 +0700

    add get by id

--- dataAccess/v1/subscriptionTier.js
+++ dataAccess/v1/subscriptionTier.js
@@ -4,3 +4,11 @@ exports.getAll = async () => {
   const result = await db.SubscriptionTier.findAll({ raw: true });
   return result;
 };
+
+exports.getByCode = async (tierCode) => {
+  const result = await db.SubscriptionTier.findOne({
+    where: { tierCode },
+    raw: true,
+  });
+  return result;
+};
