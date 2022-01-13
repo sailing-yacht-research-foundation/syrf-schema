ba5022a64cfb9af1a6f704925b536d8bc69af1e1
AlvinJK <ajk.alvin@gmail.com>
2022-01-13 12:53:15 +0700

    Added DAL for life raft

---
+++ dataAccess/v1/vesselLifeRaft.js
@@ -0,0 +1,64 @@
+const db = require('../../index');
+
+exports.create = async (data, transaction) => {
+  return await db.VesselLifeRaft.create(data, {
+    validate: true,
+    transaction,
+  });
+};
+
+exports.update = async (id, data, transaction) => {
+  const [updateCount, updatedData] = await db.VesselLifeRaft.update(data, {
+    where: {
+      id,
+    },
+    returning: true,
+    transaction,
+  });
+
+  return { updateCount, updatedData };
+};
+
+exports.getAllByVessel = async (vesselId) => {
+  const result = await db.VesselLifeRaft.findAll({
+    where: {
+      vesselId,
+    },
+    attributes: [
+      'id',
+      'serialNumber',
+      'capacity',
+      'manufacturer',
+      'model',
+      'container',
+      'lastServiceDate',
+      'manufactureDate',
+      'ownership',
+      'verifyDate',
+      'verifierUserId',
+    ],
+  });
+
+  return result;
+};
+
+exports.getById = async (id) => {
+  const result = await db.VesselLifeRaft.findByPk(id);
+  let data = result?.toJSON();
+  return data;
+};
+
+exports.delete = async (id, transaction) => {
+  const data = await db.VesselLifeRaft.findByPk(id);
+
+  if (data) {
+    await db.VesselLifeRaft.destroy({
+      where: {
+        id: id,
+      },
+      transaction,
+    });
+  }
+
+  return data?.toJSON();
+};
