5d3b041bdcb7fdf6d1bdf92965b19d1fbadceed7
AlvinJK <ajk.alvin@gmail.com>
2022-01-31 16:25:42 +0700

    Add enum for queue

--- enums.js
+++ enums.js
@@ -170,6 +170,7 @@ const bullQueues = {
   eventStop: 'event_stop',
   importTrack: 'import_track',
   calculateImport: 'calculate_import_track',
+  calculateScraped: 'calculate_scraped_track',
 };
 
 const externalServiceSources = {
