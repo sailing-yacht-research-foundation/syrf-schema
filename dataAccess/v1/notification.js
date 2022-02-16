0e2809bd08c4bb4862b23c863928b9d733fd21c0
AlvinJK <ajk.alvin@gmail.com>
2022-02-16 10:44:45 +0700

    Fix Notification fetch

--- dataAccess/v1/notification.js
+++ dataAccess/v1/notification.js
@@ -3,11 +3,21 @@ const db = require('../../index');
 exports.getNotifications = async (userId, pagination) => {
   const result = await db.UserNotification.findAllWithPaging(
     {
+      attributes: [
+        'id',
+        'userId',
+        'notificationType',
+        'notificationTitle',
+        'notificationMessage',
+        'metadata',
+        'createdAt',
+        'readAt',
+      ],
       where: {
         userId,
       },
     },
-    pagination,
+    { ...pagination, sort: 'createdAt' },
   );
 
   return result;
