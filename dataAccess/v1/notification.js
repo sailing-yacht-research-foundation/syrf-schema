1dc09ff61049bd5bc15f1de3ec6d260ba88bbaac
AlvinJK <ajk.alvin@gmail.com>
2022-02-16 10:25:04 +0700

    Init DAL

---
+++ dataAccess/v1/notification.js
@@ -0,0 +1,31 @@
+const db = require('../../index');
+
+exports.getNotifications = async (userId, pagination) => {
+  const result = await db.UserNotification.findAllWithPaging(
+    {
+      where: {
+        userId,
+      },
+    },
+    pagination,
+  );
+
+  return result;
+};
+
+exports.getById = async (id, userId) => {
+  const result = await db.UserNotification.findByPk(id, {
+    where: {
+      id,
+      userId,
+    },
+  });
+
+  return result?.toJSON();
+};
+
+exports.addNewNotification = async (data, transaction) => {
+  return await db.UserNotification.create(data, {
+    transaction,
+  });
+};
