3824dd06d482d84155f2027fafb6cf0065ab022c
AlvinJK <ajk.alvin@gmail.com>
2022-03-10 10:56:28 +0700

    Fix index creation, cast as geography to yield better cost

--- migrations/20220310024857-add-last-location-index.js
+++ migrations/20220310024857-add-last-location-index.js
@@ -1,26 +1,25 @@
 'use strict';
 
 const tableName = 'UserProfiles';
+const indexName = 'user_last_location_geog_index';
 
 module.exports = {
   up: async (queryInterface) => {
     await queryInterface.sequelize.transaction(async (transaction) => {
       const table = await queryInterface.describeTable(tableName);
       if (table.lastLocation) {
-        await queryInterface.addIndex(tableName, ['lastLocation'], {
-          unique: false,
-          type: 'SPATIAL',
-          using: 'GIST',
-          name: 'user_last_location_index',
-          transaction,
-        });
+        // Note: Require to use this, creating index like competition unit start location doesn't reduce the cost of query used as we store in geometry, not geography
+        await queryInterface.sequelize.query(
+          `CREATE INDEX IF NOT EXISTS ${indexName} ON "${tableName}" USING gist( ("lastLocation"::geography) )`,
+          { transaction },
+        );
       }
     });
   },
 
   down: async (queryInterface) => {
     await queryInterface.sequelize.transaction(async (transaction) => {
-      await queryInterface.removeIndex(tableName, 'user_last_location_index', {
+      await queryInterface.removeIndex(tableName, indexName, {
         transaction,
       });
     });
