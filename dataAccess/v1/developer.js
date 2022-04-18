512da66ed387111688a62c6e2442dd5e44e93e14
Aan Fadhil <aanfadhil16@gmail.com>
2022-04-18 14:15:24 +0700

    Feature : SNS-865 Add Developer Flag (#148)
    * add developer flag
    add relation to existing developer object
    
    * add developer data access
    
    * update model version
    
    * add delete developer

---
+++ dataAccess/v1/developer.js
@@ -0,0 +1,71 @@
+const uuid = require('uuid');
+const db = require('../../index');
+
+exports.getById = async (id, transaction) => {
+  return db.Developer.findByPk(id, { raw: true, transaction });
+};
+
+/**
+ *
+ * @param {*} userProfile
+ * @param {*} transaction
+ * @returns
+ */
+exports.create = async ({ id, name, email } = {}, transaction) => {
+  const developer = await db.Developer.create(
+    {
+      id: uuid.v4(),
+      name,
+      email,
+    },
+    {
+      transaction,
+    },
+  );
+
+  return await db.UserProfile.update(
+    {
+      developerAccountId: developer.toJSON().id,
+    },
+    {
+      where: {
+        id,
+      },
+    },
+  );
+};
+
+exports.delete = async (id, transaction) => {
+  const relatedUser = (
+    await db.UserProfile.findOne({
+      where: {
+        developerAccountId: id,
+      },
+      transaction,
+    })
+  )?.toJSON();
+
+  return (
+    await Promise.all([
+      db.Developer.destroy({
+        where: {
+          id,
+        },
+        transaction,
+      }),
+      relatedUser?.id
+        ? db.UserProfile.update(
+            {
+              developerAccountId: null,
+            },
+            {
+              where: {
+                id: relatedUser.id,
+              },
+              transaction,
+            },
+          )
+        : Promise.resolve(),
+    ])
+  )[0];
+};
