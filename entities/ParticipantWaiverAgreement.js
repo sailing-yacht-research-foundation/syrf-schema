5837ed6a0548a685676552815fa2da6f89a402c8
AlvinJK <ajk.alvin@gmail.com>
2022-04-28 16:49:32 +0700

    Remove timestamp fix

--- entities/ParticipantWaiverAgreement.js
+++ entities/ParticipantWaiverAgreement.js
@@ -38,6 +38,7 @@ module.exports = (sequelize) => {
     {
       modelName: 'ParticipantWaiverAgreement',
       sequelize,
+      timestamps: false,
     },
   );
   return ParticipantWaiverAgreement;
