5b83fa453c77b8f35725849360057b96559de323
Aan Fadhil <aanfadhil16@gmail.com>
2022-06-03 16:24:36 +0700

    Bugfix : event end location (#186)
    * add bulkCreated allowNull: false
    
    * update default bulkCreated on migration

--- migrations/20220602071015-add-vessel-default-bulkCreated-value.js
+++ migrations/20220602071015-add-vessel-default-bulkCreated-value.js
@@ -1,6 +1,17 @@
 'use strict';
+
+/**
+ * @typedef {import('sequelize').Sequelize} Sequelize
+ * @typedef {import('sequelize').QueryInterface} QueryInterface
+ */
+
 const table = 'Vessels';
 module.exports = {
+  /**
+   * @param {QueryInterface} queryInterface
+   * @param {Sequelize} Sequelize
+   * @returns
+   */
   up: async (queryInterface, Sequelize) => {
     let tableInfo = await queryInterface.describeTable(table);
 
@@ -8,15 +19,31 @@ module.exports = {
       tableInfo.bulkCreated &&
       typeof tableInfo.bulkCreated.defaultValue !== 'boolean'
     ) {
-      await queryInterface.changeColumn(table, 'bulkCreated', {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
+      await queryInterface.sequelize.transaction(async (transaction) => {
+        await queryInterface.sequelize.query(
+          `update "Vessels" set "bulkCreated" = false where "bulkCreated" is null`,
+          {
+            transaction,
+          },
+        );
+
+        await queryInterface.changeColumn(
+          table,
+          'bulkCreated',
+          {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+          },
+          {
+            transaction,
+          },
+        );
       });
     }
   },
 
-  down: async (queryInterface) => {
+  down: async (queryInterface, Sequelize) => {
     let tableInfo = await queryInterface.describeTable(table);
 
     if (
