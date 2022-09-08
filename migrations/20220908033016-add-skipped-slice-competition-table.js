095af40221df34de0d96b7ea59f44961a5a54418
AlvinJK <ajk.alvin@gmail.com>
2022-09-08 10:34:50 +0700

    Added migration

---
+++ migrations/20220908033016-add-skipped-slice-competition-table.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const tableName = 'SkippedSliceCompetitions';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      let table;
+      try {
+        table = await queryInterface.describeTable(tableName);
+      } catch (err) {
+        table = null;
+      }
+      if (!table) {
+        await queryInterface.createTable(
+          tableName,
+          {
+            id: {
+              type: Sequelize.DataTypes.UUID,
+              defaultValue: Sequelize.DataTypes.UUIDV1,
+              allowNull: false,
+              primaryKey: true,
+            },
+            competitionUnitId: {
+              type: Sequelize.DataTypes.UUID,
+              allowNull: false,
+            },
+            totalFileCount: {
+              type: Sequelize.DataTypes.INTEGER,
+              allowNull: false,
+            },
+            message: {
+              type: Sequelize.DataTypes.TEXT,
+              allowNull: false,
+            },
+            createdAt: {
+              type: Sequelize.DataTypes.DATE,
+              allowNull: false,
+              defaultValue: Sequelize.DataTypes.NOW,
+            },
+          },
+          {
+            transaction,
+          },
+        );
+      }
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      let table;
+      try {
+        table = await queryInterface.describeTable(tableName);
+      } catch (err) {
+        table = null;
+      }
+      if (table) {
+        await queryInterface.dropTable(tableName, {
+          transaction,
+        });
+      }
+    });
+  },
+};
