535c33afb49ed94b64b44afc27274f5ec29ed6a8
AlvinJK <ajk.alvin@gmail.com>
2022-09-08 11:55:45 +0700

    Add test

---
+++ __tests__/dataAccess/skippedSliceCompetition.test.js
@@ -0,0 +1,64 @@
+const { faker } = require('@faker-js/faker');
+const uuid = require('uuid');
+
+const {
+  create,
+  checkSkippedCompetition,
+} = require('../../dataAccess/v1/skippedSliceCompetition');
+
+const db = require('../../index');
+
+describe('Skipped Slice Competition DAL', () => {
+  const mockData = {
+    id: uuid.v4(),
+    competitionUnitId: uuid.v4(),
+    totalFileCount: faker.datatype.number({ min: 100 }),
+    message: 'File exceeded slice limit of 10.',
+    createdAt: new Date(),
+  };
+  const mockTransaction = db.sequelize.transaction();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('create', () => {
+    it('should call create on ScrapedSuccessfulUrl table', async () => {
+      db.SkippedSliceCompetition.create.mockResolvedValueOnce(mockData);
+      const result = await create(mockData, mockTransaction);
+
+      expect(result).toEqual(mockData);
+      expect(db.SkippedSliceCompetition.create).toHaveBeenCalledWith(mockData, {
+        validate: true,
+        transaction: mockTransaction,
+      });
+    });
+  });
+
+  describe('checkSkippedCompetition', () => {
+    it('should return true when data is found', async () => {
+      db.SkippedSliceCompetition.findOne.mockResolvedValueOnce(mockData);
+      const result = await checkSkippedCompetition(mockData.competitionUnitId);
+
+      expect(result).toEqual(true);
+      expect(db.SkippedSliceCompetition.findOne).toHaveBeenCalledWith({
+        where: {
+          competitionUnitId: mockData.competitionUnitId,
+        },
+      });
+    });
+    it('should return false when data is not found', async () => {
+      db.SkippedSliceCompetition.findOne.mockResolvedValueOnce(undefined);
+      const result = await checkSkippedCompetition(mockData.competitionUnitId);
+
+      expect(result).toEqual(false);
+      expect(db.SkippedSliceCompetition.findOne).toHaveBeenCalledWith({
+        where: {
+          competitionUnitId: mockData.competitionUnitId,
+        },
+      });
+    });
+  });
+});
